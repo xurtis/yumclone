47951474663523e56f05bc42df8558e8c3867466
Curtis Millar <curtis@curtism.me>
2018-02-11 18:06:24 +1100

    Read repository data.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate serde;
+#[macro_use]
+extern crate serde_derive;
+extern crate serde_xml_rs;
+extern crate tree_magic;
+extern crate flate2;
+
+mod repomd;
+mod primary;
+
 fn main() {
-    println!("Hello, world!");
 }
